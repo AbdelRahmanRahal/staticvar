e43fa7d92c79c44be04eddfb5626c8714a96aa8f
AbdelRahman <abdelrahman.rahalll@gmail.com>
2023-04-14 14:32:39 +0200

    Added a license file and organised the package more

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.2' 
+VERSION = '0.0.4' 
 DESCRIPTION = 'Static variables for Python'
 LONG_DESCRIPTION = 'Static variables for Python like in C'
 
@@ -11,14 +11,17 @@ setup(
         author_email="<abdelrahman.rahal.mail@gmail.com>",
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
+        url="https://github.com/AbdelRahmanRahal/StaticVar",
         packages=find_packages(),
         install_requires=['varname'],
         python_requires=">=3.10",
+
         
         keywords=['Static', 'Static Variables', 'StaticVar'],
         classifiers= [
             "Development Status :: 3 - Alpha",
             "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
             "Programming Language :: Python :: 3 :: Only",
             "Operating System :: MacOS :: MacOS X",
             "Operating System :: Microsoft :: Windows",
