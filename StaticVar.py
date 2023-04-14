e93cf0245ce0a7673e77c88baaa01ada3d5ecb1a
AbdelRahman <abdelrahman.rahalll@gmail.com>
2023-04-14 05:51:35 +0200

    complete change to logic, looks better too

--- StaticVar.py
+++ StaticVar.py
@@ -1,22 +1,18 @@
-from Errors import *
+from varname import varname
+
+StaticVars = dict()
 
 class Static():
-	__value = None
-	def __init__(self, value, vtype = "any"):
-		if "__value" not in self.__dict__:
-			if vtype == "any":
+	def __init__(self, value, vtype = "infer"):
+		self.__name = varname()
+
+		if self.__name not in StaticVars:
+			if vtype == "infer":
 				self.__value = value
-				match str(type(value)):
-					case "<class 'int'>":
-						self.__type = "int"
-					case "<class 'float'>":
-						self.__type = "float"
-					case "<class 'str'>":
-						self.__type = "str"
-					case "<class 'bool'>":
-						self.__type = "bool"
-					case _:
-						raise InvalidTypeError(type(value))
+				if str(type(value).__name__) in ("int", "float", "str", "bool"):
+					self.__type = str(type(value).__name__)
+				else:
+					raise InvalidTypeError(str(type(value).__name__))
 			else:
 				self.__type = vtype
 				match self.__type:
@@ -47,34 +43,53 @@ class Static():
 					case _:
 						raise InvalidTypeError(f"\"{vtype}\"")
 
+			StaticVars[self.__name] = (self.__value, self.__type)
+
+		else:
+			self.__value = StaticVars[self.__name][0]
+			self.__type = StaticVars[self.__name][1]
+
+
 	def set(self, value):
-		match self.__type:
-				case "int":
-					try:
-						self.__value = int(value)
+		match StaticVars[self.__name][1]:
+			case "int":
+				try:
+					StaticVars[self.__name] = (int(value), "int")
 
-					except ValueError as VE:
-						raise VE
-				case "float":
-					try:
-						self.__value = float(value)
+				except ValueError as VE:
+					raise VE
+			case "float":
+				try:
+						StaticVars[self.__name] = (float(value), "float")
 
-					except ValueError as VE:
-						raise VE
-				case "str":
-					try:
-						self.__value = str(value)
+				except ValueError as VE:
+					raise VE
+			case "str":
+				try:
+					StaticVars[self.__name] = (str(value), "str")
 
-					except ValueError as VE:
-						raise VE
-				case "bool":
-					try:
-						self.__value = bool(value)
+				except ValueError as VE:
+					raise VE
+			case "bool":
+				try:
+					StaticVars[self.__name] = (bool(value), "bool")
 
-					except ValueError as VE:
-						raise VE
+				except ValueError as VE:
+					raise VE
+
+		self.__value = StaticVars[self.__name][0]
 
 		return self.__value
+
 
 	def get(self):
-		return self.__value
+		return StaticVars[self.__name][0]
+
+	def getType(self):
+		return StaticVars[self.__name][1]
+
+
+class InvalidTypeError(Exception):
+	def __init__(self, vtype):
+		self.type = vtype
+		super().__init__(f"Unsupported data type entered: {self.type}")
