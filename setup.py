2a9d3b2e0db5b745784d7f5266020b055289d21e
AbdelRahman <abdelrahman.rahalll@gmail.com>
2023-04-14 18:10:31 +0200

    changed the build compiler thing to Poetry

--- setup.py
+++ setup.py
@@ -1,23 +1,29 @@
 from setuptools import setup, find_packages
+from pathlib import Path
 
-VERSION = '0.0.4' 
-DESCRIPTION = 'Static variables for Python'
-LONG_DESCRIPTION = 'Static variables for Python like in C'
+VERSION = "0.0.5"
+DESCRIPTION = "The horrors of C Static variables for Python, made with Python."
+THIS_DIRECTORY = Path(__file__).parent
+LONG_DESCRIPTION = (THIS_DIRECTORY / "README.md").read_text(encoding = "utf8")
 
 setup(
-        name="StaticVar", 
-        version=VERSION,
-        author="AbdelRahman Rahal",
-        author_email="<abdelrahman.rahal.mail@gmail.com>",
-        description=DESCRIPTION,
-        long_description=LONG_DESCRIPTION,
-        url="https://github.com/AbdelRahmanRahal/StaticVar",
-        packages=find_packages(),
-        install_requires=['varname'],
-        python_requires=">=3.10",
+        name = "StaticVar", 
+        version = VERSION,
+        author = "AbdelRahman Rahal",
+        author_email = "<abdelrahman.rahal.mail@gmail.com>",
+        description = DESCRIPTION,
+        long_description = LONG_DESCRIPTION,
+        long_description_content_type = "text/markdown",
+        url = "https://github.com/AbdelRahmanRahal/StaticVar",
+        project_urls = {
+            'Repository': "https://github.com/AbdelRahmanRahal/StaticVar"
+        },
+        packages = find_packages(),
+        install_requires = ["varname"],
+        python_requires = ">=3.10",
 
         
-        keywords=['Static', 'Static Variables', 'StaticVar'],
+        keywords=["Static", "Static Variables", "StaticVar"],
         classifiers= [
             "Development Status :: 3 - Alpha",
             "Intended Audience :: Developers",
--- src/StaticVar/__init__.py
+++
@@ -1,1 +0,0 @@
-from StaticVar import Static
