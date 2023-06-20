b08e4d7fd9cac8455436a103355cfbfc2afb88c8
AbdelRahman <abdelrahman.rahal.mail@gmail.com>
2023-06-20 05:18:33 +0300

    Renewed the logic. We're using decorators now.

---
+++ staticvar/staticvar.py
@@ -0,0 +1,32 @@
+from typing import Any, Callable
+from functools import wraps
+
+def staticvar(var_name: str, initial_value: Any, var_type: type = Any) -> Callable:
+	'''
+	Decorator to create a static variable for the given function
+
+	Args:
+		var_name (str): static variable name.
+		initial_value (Any): initial value for the variable.
+		var_type (type): type of the variable. Leave empty to make it dynamic.
+
+	Returns:
+		Callable: The decorated function.
+
+	Raises:
+		TypeError: If a type is specified and it does not match the initial value.
+
+	'''
+	
+	def decorator(func):
+		if var_type != Any and not isinstance(initial_value, var_type):
+			raise TypeError(f"{func.__name__}.{var_name} must be of type {var_type}. Current type: {initial_value.__class__}")
+		
+		setattr(func, var_name, initial_value)
+		
+		@wraps(func)
+		def wrapper(*args, **kwargs):
+			return func(*args, **kwargs)
+		
+		return wrapper
+	return decorator
