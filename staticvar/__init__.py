9653befd0b943c3ef2bd819da9494250b68e65e1
AbdelRahman <abdelrahman.rahal.mail@gmail.com>
2023-06-25 10:25:51 +0300

    Update __init__.py

--- staticvar/__init__.py
+++ staticvar/__init__.py
@@ -1,3 +1,10 @@
 from .staticvar import staticvar
+from .exceptions import (
+    StaticvarException,
+    IllegalInstantiationError,
+    UnsupportedTypeError
+)
+from .utils import Configure, StaticvarExceptionHandler
+
 
 __version__ = "0.1.0"
